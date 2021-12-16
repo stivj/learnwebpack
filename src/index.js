9df7e824708257b1ab85fb99101a0696b067f01b
stivj <stiven.danilov@tptlive.ee>
2021-12-16 17:41:39 +0200

    add todo app

--- src/index.js
+++ src/index.js
@@ -5,18 +5,33 @@ console.log('Hello smthing');
 import './style.scss';
 import Vue from 'vue/dist/vue.common.dev';
 
+
 new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue'
+        message: 'Hello Vue',
+        items: ['sai', 'piim']
+    },
+    computed: {
+        reversedMessage(){
+            return this.message.split('').reverse().join('');
+        }
+    },
+    methods: {
+        addItem(){
+            if(this.message.trim() !== ''){
+            this.items.push(this.message);
+            }
+            this.message = '';
+        }
     }
 });
 
-document.querySelector('#input').addEventListener('input', function(event){
-    document.querySelector('#paragraph').innerHTML = event.target.value;
-});
+// document.querySelector('#input').addEventListener('input', function(event){
+//     document.querySelector('#paragraph').innerHTML = event.target.value;
+// });
 
-document.querySelector('#button').addEventListener('click', function(){
-    document.querySelector('#input').value = '';
-    document.querySelector('#paragraph').innerHTML = '';
-});
+// document.querySelector('#button').addEventListener('click', function(){
+//     document.querySelector('#input').value = '';
+//     document.querySelector('#paragraph').innerHTML = '';
+// });

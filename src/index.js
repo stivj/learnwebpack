f9948fd94190529d127315d3fb1ef4295e2417a3
stivj <stiven.danilov@tptlive.ee>
2021-12-16 14:13:30 +0200

    webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Logger from './module.js';
+
+Logger.log('Hellou modules');
+
+const Logger2 = require('./module2.js' );
+Logger2.log2('Hello require');
---
+++ src/module2.js
@@ -0,0 +1,5 @@
+module.exports = {
+    log2(message) {
+        console.log(message.toLoweCase());
+    }
+}
